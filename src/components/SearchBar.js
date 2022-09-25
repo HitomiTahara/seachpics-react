6f357c836cc17ebabdbc5bae09f68b722220fbef
HitomiTahara <hitomi.20220218@gmail.com>
2022-09-25 18:20:14 +0900

    Connecting parrents to child component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,24 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
 
-  onInputChange(event){
-    console.log(event.target.value);
-  }
-  state = { text: "" };
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="type" onChange={this.onInputChange} />
+            <input
+              type="type"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
