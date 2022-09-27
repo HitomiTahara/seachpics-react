25706cef2bbf1ee826f73d5107d70030bf092c9b
HitomiTahara <hitomi.20220218@gmail.com>
2022-09-27 16:05:57 +0900

    Create axios.create to separate

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,22 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  state = { images: [] };
+  onSearchSubmit = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:"Client-ID R1Og89X6qwG-SV4uuUvVkA9YdVtYMbQ0cVqARJvaPjM",
+      },
+    });
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found:{this.state.images.length}images
       </div>
     );
   }
